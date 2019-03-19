aef8206ce15bd409174ba7c7342e78f3a121e11b
Arpithasj97 <arpithasjabhi@gmail.com>
2019-03-19 11:14:18 +0530

    done

---
+++ LMS/src/User.java
@@ -0,0 +1,38 @@
+
+
+public class User {
+	private String uname;
+    private String pwd;
+    public boolean valid;
+	
+	
+    public String getuName() {
+       return uname;
+	}
+
+    public void setuName(String uname) {
+       uname = uname;
+	}
+
+	
+    public String getpwd() {
+       return pwd;
+			}
+
+    public void setpwd(String pwd) {
+       pwd = pwd;
+			}
+			
+    public boolean isValid() {
+        return valid;
+	}
+
+     public void setValid(boolean newValid) {
+        valid = newValid;
+	}
+   	
+			
+   
+	}
+
+
